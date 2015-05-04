8bb61acc5a6a26e9f3453c502a6fa3b0e00be8a5
streaky <streaky@mybrokenlogic.com>
2015-05-04 16:23:10 +0100

    Add initial tests bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+define("SAG_REPO_BASE", realpath(__DIR__."/../"));
+
+require_once(SAG_REPO_BASE."/vendor/autoload.php");
