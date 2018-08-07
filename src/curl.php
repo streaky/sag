43f4ebe9f6ba8582ffca2eb526d0dd73b1ca1428
Martin Nicholls <streaky@mybrokenlogic.com>
2018-08-07 20:07:04 +0100

    Fix a PHP 7 fail.

--- src/curl.php
+++ src/curl.php
@@ -337,7 +337,7 @@ class curl {
 				} else {
 					$line = explode(':', $respHeaders[$i], 2);
 					$line[0] = strtolower($line[0]);
-					$response->headers->$line[0] = ltrim($line[1]);
+					$response->headers->{$line[0]} = ltrim($line[1]);
 
 					if($line[0] == 'set-cookie') {
 						$response->cookies = $this->parseCookieString($line[1]);
